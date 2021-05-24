d9b3d21af45be8536f9e273e5ba70cbd4e4388cb
thao15059 <thao15059@gmail.com>
2021-05-24 21:27:53 +0700

    implements register user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/").post(registerUser);
 router.route("/login").post(authUser);
 router.route("/profile").get(protect, getUserProfile);
 
