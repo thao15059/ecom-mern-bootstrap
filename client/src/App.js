961919a17b9d7b059b0b3c384d52af2d03dbd8ec
thao15059 <thao15059@gmail.com>
2021-05-31 19:03:07 +0700

    implements register with redux and register page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProductPage from "./pages/ProductPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Container>
           <Route exact path="/" component={HomePage} />
           <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
           <Route path="/product/:id" component={ProductPage} />
           <Route path="/cart/:id?" component={CartPage} />
         </Container>
